516f79cbe2b79cf7c6c9d7ba5450478b8a62114a
Kotkoa <kotkoa@gmail.com>
2022-08-05 00:15:53 +0200

    add metricSelector component to get out logic

--- components/Converter/index.js
+++ components/Converter/index.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react'
 import InputBorder from '../UI/InputBorder'
 import Button from '../UI/Button'
+import MetricSelector from '../UI/MetricSelector'
 
 const Converter = (props) => {
   const [metricWeight, setMetricWeight] = useState(true)
-  const [metricHeight, setMetricHeight] = useState(true)
 
-  const HandleClickWeight = () =>
-    setMetricWeight((prevState) => {
-      return !prevState
-    })
-
-  const HandleClickHeight = () =>
-    setMetricHeight((prevState) => {
-      return !prevState
-    })
+  const changeMetric = () => setMetricWeight((prevState) => !prevState)
 
   return (
     <div className="flex flex-col items-center justify-center flex-1 py-16">
@@ -23,32 +15,8 @@ const Converter = (props) => {
           name="Weight"
           className="flex flex-row justify-between mb-5"
         >
-          <input type="text" className="w-5 bg-white" />
-          <div className="inline-flex space-x-2">
-            <Button onClick={HandleClickWeight} view={metricWeight}>
-              CM
-            </Button>
-            <Button onClick={HandleClickWeight} view={!metricWeight}>
-              IN
-            </Button>
-          </div>
-        </InputBorder>
-        <InputBorder
-          name="Height"
-          className="flex flex-row items-center justify-between"
-        >
-          <div>
-            <input type="text" className="w-5 bg-white" />
-            {!metricHeight && <input type="text" className="w-5 bg-white" />}
-          </div>
-          <div className="inline-flex space-x-2">
-            <Button onClick={HandleClickHeight} view={metricHeight}>
-              CM
-            </Button>
-            <Button onClick={HandleClickHeight} view={!metricHeight}>
-              IN
-            </Button>
-          </div>
+          <input type="text" className="w-full bg-white outline-none" />
+          <MetricSelector changeMetric={changeMetric} isMetric={metricWeight} />
         </InputBorder>
       </form>
     </div>
--- components/UI/InputBorder/index.js
+++ components/UI/InputBorder/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 const InputBorder = (props) => {
+  const { name, error, className, children } = props
+
   return (
     <div className="flex flex-col justify-start">
       <label className="capitalize neutral-600 after:content-['*'] after:ml-0.5 after:text-red-800 mb-2 font-light text-base">
-        {props.name || InputBorder}
+        {name || InputBorder}
       </label>
       <div
-        className={`w-[446px] rounded-xl p-2 border border-neutral-300 bg-white text-gray-900 invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-neutral-300 disabled:bg-zinc-100 disabled:text-zinc-500 disabled:shadow-none ${props.className}`}
+        className={`w-[446px] rounded-xl p-2 border border-neutral-300 bg-white text-gray-900 invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-neutral-300 disabled:bg-zinc-100 disabled:text-zinc-500 disabled:shadow-none ${className}`}
       >
-        {props.children}
+        {children}
       </div>
+      {error && <p className="text-red-800">{error}</p>}
     </div>
   )
 }
