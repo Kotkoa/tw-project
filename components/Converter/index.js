60d88379700b8d805b830f610bbc8b8999dae2ed
Kotkoa <kotkoa@gmail.com>
2022-08-12 11:33:29 +0200

    weight

--- components/Converter/index.js
+++ components/Converter/index.js
@@ -5,8 +5,32 @@ import MetricSelector from '../UI/MetricSelector'
 
 const Converter = (props) => {
   const [metricWeight, setMetricWeight] = useState(true)
+  const [inputWeight, setInputWeight] = useState('')
 
   const changeMetric = () => setMetricWeight((prevState) => !prevState)
+
+  const handleChangeWeight = ({ target: { value } }) => {
+    if (metricWeight) {
+      return setInputWeight(value)
+    }
+
+    if (!metricWeight) {
+      const weightLBStoKG = Math.floor(inputWeight * 2.2)
+      return setInputWeight(weightLBStoKG)
+    }
+  }
+
+  //finish at this place
+  const displayWeight = () => {
+    if (metricWeight) {
+      return inputWeight
+    }
+
+    if (!metricWeight) {
+      const weightKGtoLBS = Math.floor(inputWeight * 2.2)
+      return weightValueLBS
+    }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center flex-1 py-16">
@@ -15,7 +39,13 @@ const Converter = (props) => {
           name="Weight"
           className="flex flex-row justify-between mb-5"
         >
-          <input type="text" className="w-full bg-white outline-none" />
+          <input
+            value={displayWeight}
+            onChange={handleChangeWeight}
+            placeholder="0"
+            type="text"
+            className="w-full bg-white outline-none"
+          />
           <MetricSelector changeMetric={changeMetric} isMetric={metricWeight} />
         </InputBorder>
       </form>
