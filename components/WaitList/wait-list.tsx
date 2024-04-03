11d50bed98443f43ce94d861eb240d9d18b352cf
Kotkoa <kotkoa@gmail.com>
2024-04-04 00:16:32 +0200

    Add waitlist functionality to WaitList component

--- components/WaitList/wait-list.tsx
+++ components/WaitList/wait-list.tsx
@@ -1,45 +1,128 @@
-import React, { FC } from 'react';
-import Script from 'next/script';
+import React, { FC, useState } from 'react';
+import { validateEmail } from '@/utils/validate-email';
+import ClipLoader from 'react-spinners/ClipLoader';
+
+type WaitlistData = {
+  email: string;
+  waitlist_id?: number;
+  referral_link?: string;
+};
+
+type WaitlistResponse = {
+  priority: number | string;
+  referral_link: string;
+  total_referrals: number;
+};
 
 export const WaitList: FC = () => {
+  const [waitlistData, setWaitlistData] = useState<WaitlistResponse | null>(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submitWaitlist = (data: WaitlistData) => {
+    if (!data.email) {
+      setError('Please enter your email');
+      return;
+    }
+    if (validateEmail(data.email) === false) {
+      setError('Please enter a valid email');
+      return;
+    }
+
+    setLoading(true);
+
+    data.waitlist_id = 15316;
+    data.referral_link = document.URL;
+
+    fetch('https://api.getwaitlist.com/api/v1/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setWaitlistData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="mt-10">
-      <div className="mb-8">
-        <label htmlFor="hs-leading-icon" className="mb-2 block text-sm font-medium dark:text-white">
-          Email address
-        </label>
-        <div className="relative">
-          <input
-            type="text"
-            id="hs-leading-icon"
-            name="hs-leading-icon"
-            className="block w-full rounded-lg border-gray-200 px-4 py-3 ps-11 text-sm shadow-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
-            placeholder="you@site.com"
-          />
-          <div className="pointer-events-none absolute inset-y-0 start-0 z-20 flex items-center ps-4">
-            <svg
-              className="size-4 flex-shrink-0 text-gray-400 dark:text-gray-600"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+    <div className="flex h-full w-full p-6">
+      {!waitlistData ? (
+        <form className="w-full">
+          <div className="flex flex-col space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="email" className="text-gray-700">
+                What is your email?
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Please enter your email"
+                autoComplete="email"
+                onChange={(e) => e.stopPropagation()}
+                required
+                className="rounded-md border border-gray-200 p-2 text-base text-gray-700"
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <button
+              type="button"
+              disabled={loading}
+              onClick={() => {
+                const emailElement = document.getElementById('email') as HTMLInputElement;
+                if (emailElement) {
+                  submitWaitlist({
+                    email: emailElement.value,
+                  });
+                } else {
+                  console.error('Email input element not found');
+                }
+              }}
+              className="w-full rounded-md bg-indigo-600 px-3 py-2 text-white transition duration-300 hover:bg-indigo-700"
             >
-              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-            </svg>
+              {loading ? (
+                <ClipLoader size={25} color={'#ffffff'} loading={true} />
+              ) : (
+                'Sign up for the waitlist'
+              )}
+            </button>
+            {error && <div className="mt-2 px-6 text-center text-xs text-red-500">{error}</div>}
+          </div>
+        </form>
+      ) : (
+        <div className="text-gray-700">
+          <div>
+            Thank you for signing up. Your are waiter <b>{waitlistData.priority}</b> on the
+            waitlist.{' '}
+          </div>
+          <div>
+            Referral link is: <b>{waitlistData.referral_link}</b>
+          </div>
+          <div>
+            Total referrals: <b>{waitlistData.total_referrals}</b>
+          </div>
+          <div className="mt-4">
+            <button
+              type="button"
+              disabled={loading}
+              onClick={() => {
+                setWaitlistData(null);
+              }}
+              className="w-full rounded-md bg-indigo-600 px-3 py-2 text-white transition duration-300 hover:bg-indigo-700"
+            >
+              Return to signup
+            </button>
           </div>
         </div>
-      </div>
-      <div id="getWaitlistContainer" data-waitlist_id="15316" data-widget_type="WIDGET_1"></div>
-      <Script
-        src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"
-        strategy="afterInteractive"
-      />
+      )}
     </div>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
