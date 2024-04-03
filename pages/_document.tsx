1902eeb59844d43a9a65250913fe60a7a991d0c6
Kotkoa <kotkoa@gmail.com>
2024-04-03 22:05:25 +0200

    Add WaitList component and script import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
